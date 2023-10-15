a52c516f717a13d8fc3eddebe72e9627e4f63459
221220000-Chenyani <1456492382@qq.com>
2023-10-15 17:23:05 +0800

    finish

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -1,11 +1,11 @@
 // tests5.rs
-unsafe fn modify_by_address(mut address: usize) -> u32 {
+unsafe fn modify_by_address(address: usize) {
     // TODO: Fill your safety notice of the code block below to match your
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
     unsafe {
-    	    let mut t : u32 = 0xAABBCCDD;
-    	    t
+    	    let ptr = address as *mut u32;
+            *ptr = 0xAABBCCDD;
     }
 }
 
@@ -14,11 +14,10 @@ mod tests {
     use super::*;
     #[test]
     fn test_success() {
-        let mut t: u32 = 0x11223344;
+        let mut t: u32 = 0x12345678;
         // SAFETY: The address is guaranteed to be valid and contains
         // a unique reference to a `u32` local variable.
-        unsafe { t = modify_by_address(&mut t as *mut u32 as usize) };
-        println!("{:x}",t);
+        unsafe { modify_by_address(&mut t as *mut u32 as usize) };
         assert!(t == 0xAABBCCDD);
     }
 }
